5990413d4772256730bfd3c8ee17616a58a8daf8
farahnurlaili <138429805+farahnurlaili@users.noreply.github.com>
2024-04-02 08:55:38 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mavens Admin"
+include(":app")
+ 
